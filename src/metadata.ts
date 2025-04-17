2151c42f77f21edc0b5948866c6ccf6d466f7b6a
MaiVietHungHE16355 <hungvietmai2k2@gmail.com>
2025-04-17 21:12:59 +0700

    fix: dev-container build and hot reload run slowly

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,5 +3,5 @@ export default async () => {
     const t = {
         ["./shared/user/schemas/user.schema.js"]: await import("./shared/user/schemas/user.schema.js")
     };
-    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./shared/user/users.controller.js"), { "UsersController": { "create": { type: t["./shared/user/schemas/user.schema.js"].User }, "findAll": { type: [t["./shared/user/schemas/user.schema.js"].User] }, "findOne": { type: Object } } }], [import("./auth/auth.controller.js"), { "AuthController": { "login": {}, "check": { type: Object } } }], [import("./health/health.controller.js"), { "HealthController": { "check": { type: Object } } }]] } };
+    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./shared/user/users.controller.js"), { "UsersController": { "create": { type: t["./shared/user/schemas/user.schema.js"].User }, "findAll": { type: [t["./shared/user/schemas/user.schema.js"].User] }, "findOne": { type: String } } }], [import("./auth/auth.controller.js"), { "AuthController": { "login": {}, "check": { type: Object } } }], [import("./health/health.controller.js"), { "HealthController": { "check": { type: Object } } }]] } };
 };
